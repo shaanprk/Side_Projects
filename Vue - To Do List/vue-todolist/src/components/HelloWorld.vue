c8193e8e244fe025c18313099457d1224a92cd21
Sean <jungbeanshaanprk@gmail.com>
2024-09-11 21:18:55 +0900

    Implementing To do list

---
+++ Vue - To Do List/vue-todolist/src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
+        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+      </form>
+
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+      </ul>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Yurr',
+      skills: [
+        { "skill": "Vue.js" },
+        { "skill": "Frontend Developer" },
+        { "skill": "Backend Developer" },
+      ],
+      showAlert: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.alert {
+  background-color: yellow;
+  width: 100%;
+  height: 30px;
+}
+</style>
